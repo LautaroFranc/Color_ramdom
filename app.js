e12fc7576bb3973ad9edcc4cc08913836e217b06
Marcel <98562281+LautaroFranc@users.noreply.github.com>
2022-03-22 22:10:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,49 +8,43 @@ let valor=1000;
 let s=0;
 let a,v,r;
 let time=0;
-
-
-
+alert("Advertencia: Puedes modificar la velocidad")
+alert("Advertencia: cuanto mas SUBA mas lento ira");
+alert("Advertencia: cuanto mas BAJE mas rapido ira");
 
 btm.addEventListener("click",(e1)=>{
-    
+
     btm.classList.add("borde");
     btm2.classList.remove("borde");
 
     btm.classList.remove("borde2");
     btm2.classList.add("borde2");
-    
-        
+
+   
+  
         if (rango.classList.contains("notacced")==false) {
   
             rango.classList.remove("accder")
             rango.classList.add("notacced")
-
         }
-        
-        
-        
+
     
     if (!time) {
         time=setInterval(
 
-            function c() {
-               if (s==60) {
-                   s=0;
-               }
-                r=Math.floor(Math.random()*90);
-                v=Math.floor(Math.random()*9);
-                a=Math.floor(Math.random()*9);
+            ()=> {
                 
+                r=Math.floor(Math.random()*10);
+                v=Math.floor(Math.random()*20);
+                a=Math.floor(Math.random()*10);
+
                 Color.style.backgroundColor=`#${r}${v}${a}`;
-                Color.innerHTML=`#${r}${v}${a}`;
-              
+                Color.innerHTML=`<p>#${r}${v}${a}</p>`;
                 s++;            
             },valor)
-            
     }
     
-})
+});
 
 
 btm2.addEventListener("click",(e2)=>{
@@ -61,29 +55,27 @@ btm2.addEventListener("click",(e2)=>{
     btm2.classList.remove("borde2");
     btm.classList.add("borde2");
 
-   
             if (rango.classList.contains("accder")==false) {
-               
-
                rango.classList.remove("notacced");
                rango.classList.add("accder");
-
-                rango.addEventListener("change",()=>{
-                    valor=rango.value;
-        
-                    if (valor<100) {
-                        rango.classList.add("min");
-                        rango.classList.remove("max");
-                    }else if (valor>500) {
-                        
-                    rango.classList.add("max");
-                    rango.classList.remove("min");
-                    }
-                 
-                })}
+            }
     
     clearInterval(time)
     
     time=null
-})
+});
 
+rango.addEventListener("change",()=>{
+    valor=rango.value;
+   
+    if (valor<100) {
+        rango.classList.add("min");
+        rango.classList.remove("max");
+    }else if (valor>500) {
+        
+    rango.classList.add("max");
+    rango.classList.remove("min");
+    }
+   
+ 
+});
